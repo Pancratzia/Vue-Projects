13a74baf4f7e8b974cb1840c7c7051046c143aa9
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-03-20 19:25:27 -0400

    Usando v-if y v-else

--- helloworld/src/App.vue
+++ helloworld/src/App.vue
@@ -5,6 +5,8 @@
 
     <CounterComposition />
 
+    <ExampleSelective />
+
     <h1>Listado de Posts:</h1>
 
     <PostContainer />
@@ -16,6 +18,7 @@
 import PostContainer from './components/PostContainer.vue';
 import CounterOption from './components/CounterOption.vue';
 import CounterComposition from './components/CounterComposition.vue';
+import ExampleSelective from './components/ExampleSelective.vue';
 </script>
 
 <style>
@@ -43,7 +46,7 @@ h1 {
   font-size: 5rem;
   font-weight: 900;
   color: #2c3e50;
-  margin-top: 5rem;
+  margin-top: 2rem;
 }
 
 .container {
---
+++ helloworld/src/components/ExampleSelective.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1 v-if="age >= 18" class="title">Eres mayor de edad</h1>
+        <h1 v-else class="title">Eres menor de edad</h1>
+        <h2 class="subtitle">Edad: {{ age }}</h2>
+    </div>
+</template>
+
+<script lang="ts" setup>
+let age = Math.floor(Math.random() * 100) + 1;
+</script>
+
+<style scoped>
+
+.title {
+    color: #2c3e50;
+}
+
+.subtitle {
+    color: blueviolet;
+    margin-top: 1rem;
+}
+</style>
