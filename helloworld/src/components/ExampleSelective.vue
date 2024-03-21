0c0fa781a8894507226673a7f4df4ebedbf53a93
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-03-21 15:02:09 -0400

    Usando el v-show, que hace un display none

--- helloworld/src/components/ExampleSelective.vue
+++ helloworld/src/components/ExampleSelective.vue
@@ -1,21 +1,13 @@
 <template>
     <div>
-        <h1 v-if="age >= 18" class="title">Eres mayor de edad</h1>
-        <h1 v-else class="title">Eres menor de edad</h1>
+        <h1 v-show="age >= 18" class="title">Eres mayor de edad</h1>
+        <h1 v-show="age < 18" class="title">Eres menor de edad</h1>
         <h2 class="subtitle">Edad: {{ age }}</h2>
     </div>
 </template>
 
-<script lang="ts">
-    import { defineComponent } from 'vue';
-    export default defineComponent({
-        name: 'ExampleSelective',
-        setup() {
-            let age = Math.floor(Math.random() * 100) + 1;
-
-            return { age };
-        }
-    })
+<script lang="ts" setup>
+let age = Math.floor(Math.random() * 100) + 1;
 </script>
 
 <style scoped>
