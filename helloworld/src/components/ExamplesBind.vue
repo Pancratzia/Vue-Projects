0e9eced5dbcacbef53307f0d241dca48246a860a
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-03-21 15:59:20 -0400

    Usando el v-on para manejar eventos

--- helloworld/src/components/ExamplesBind.vue
+++ helloworld/src/components/ExamplesBind.vue
@@ -1,12 +1,21 @@
 <template>
     <div class="mensajes">
-        <p :class="{ danger: hasError, success: !hasError }" v-bind:style="{ backgroundColor: hasError ? 'rgba(255, 0, 0, 0.4)' : 'rgba(0, 255, 0, 0.4)'}">{{ message }}</p>
+        <p :class="{ danger: hasError, success: !hasError }"
+            v-bind:style="{ backgroundColor: hasError ? 'rgba(255, 0, 0, 0.4)' : 'rgba(0, 255, 0, 0.4)' }">{{ hasError ?
+            'Se ha producido un error' : 'El mensaje se ha enviado satisfactoriamente' }}</p>
+        <button @click="handleClick">Cambiar Mensaje</button>
     </div>
 </template>
 
 <script lang="ts" setup>
-let hasError = Math.random() > 0.5;
-const message = hasError ? 'Se ha producido un error' : 'El mensaje se ha enviado satisfactoriamente';
+import { ref, Ref } from 'vue';
+let hasError: Ref<boolean> = ref(Math.random() > 0.5);
+
+const handleClick = (): void => {
+    hasError.value = !hasError.value;
+}
+
+
 </script>
 
 <style scoped>
@@ -24,5 +33,15 @@ const message = hasError ? 'Se ha producido un error' : 'El mensaje se ha enviad
 
 .success {
     color: green;
+}
+
+button {
+    padding: .5rem;
+    background-color: #2c3e50;
+    color: white;
+    border: 0;
+    outline: 0;
+    margin-top: 1rem;
+    cursor: pointer;
 }
 </style>
