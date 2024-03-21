77c212e44acfd2a82ae472afca05ff5c7347d15e
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-03-21 15:23:59 -0400

    Listando paises que empiezan por V con v-for

--- helloworld/src/App.vue
+++ helloworld/src/App.vue
@@ -7,6 +7,8 @@
 
     <ExampleSelective />
 
+    <ExampleFor />
+
     <h1>Listado de Posts:</h1>
 
     <PostContainer />
@@ -19,6 +21,7 @@ import PostContainer from './components/PostContainer.vue';
 import CounterOption from './components/CounterOption.vue';
 import CounterComposition from './components/CounterComposition.vue';
 import ExampleSelective from './components/ExampleSelective.vue';
+import ExampleFor from './components/ExampleFor.vue';
 </script>
 
 <style>
@@ -40,6 +43,7 @@ body {
   justify-content: center;
   align-items: center;
   min-height: 100vh;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
 }
 
 h1 {
---
+++ helloworld/src/components/ExampleFor.vue
@@ -0,0 +1,48 @@
+<template>
+    <h1 class="title">Paises que comienzan con V:</h1>
+    <ul class="subtitle">
+        <li v-for="(country, index) in countries" :key="index"  :class="country==='Venezuela' ? 'item-venezuela item' : 'item'" >{{ country }}</li>
+    </ul>
+</template>
+
+<script lang="ts" setup>
+import { ref, Ref } from 'vue';
+
+//Countries that start with V
+let countries:Ref<Array<string>> =ref(['Venezuela', 'Vatican City', 'Vietnam', 'Vanuatu']);
+</script>
+
+<style scoped>
+    .title{
+        font-size: 3rem;
+        margin-bottom: 1rem;
+    }
+
+    .subtitle{
+        list-style: none;
+        padding: 0;
+        margin: 0;
+        display: flex;
+        flex-direction: column;
+        gap: .5rem;
+        align-items: center;
+    }
+
+    .item{
+        font-size: 1.5rem;
+        font-weight: bold;
+        display: block;
+        color: blueviolet;
+        width: max-content;
+        text-transform: uppercase;
+    }
+
+    .item-venezuela{
+        background: linear-gradient(to right, yellow, blue, red);
+        background-size:  100% 100%;
+        color: transparent;
+        background-clip: text;
+    }
+
+
+</style>
