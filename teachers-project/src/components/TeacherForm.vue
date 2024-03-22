a9e5d7ecc1801e9982286a82644e7afa1cd3c1b0
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-03-21 21:58:23 -0400

    Se termina la construcción de la estructura de datos

--- teachers-project/src/components/TeacherForm.vue
+++ teachers-project/src/components/TeacherForm.vue
@@ -1,45 +1,67 @@
 <template>
-  <section>
-    <h3>Añadir Profesor</h3>
+    <section>
+        <h3>Añadir Profesor</h3>
 
-    <div>
-      <label for="name">Nombre</label>
-      <input id="name" type="text" placeholder="Nombre" />
-    </div>
+        <div>
+            <label for="name">Nombre</label>
+            <input id="name" type="text" placeholder="Nombre" />
+        </div>
 
-    <div>
-      <label for="apellidos">Apellidos</label>
-      <input id="apellidos" type="text" placeholder="Apellidos" />
-    </div>
+        <div>
+            <label for="apellidos">Apellidos</label>
+            <input id="apellidos" type="text" placeholder="Apellidos" />
+        </div>
 
-    <div>
-      <label for="dninif">DNI/NIF</label>
-      <input id="dninif" type="text" placeholder="DNI/NIF" />
-    </div>
+        <div>
+            <label for="dninif">DNI/NIF</label>
+            <input id="dninif" type="text" placeholder="DNI/NIF" />
+        </div>
 
-    <div>
-      <label for="materias">Materias</label>
-      <input id="materias" type="text" placeholder="Materias" />
+        <div>
+            <label for="materias">Materias</label>
+            <input id="materias" type="text" placeholder="Materias" />
 
-      <button>Anadir</button>
-    </div>
+            <button>Anadir</button>
+        </div>
 
-    <div>
-        <input type="checkbox" name="documentacionEntregada" id="documentacionEntregada" />
-        <label for="documentacionEntregada">Documentación entregada</label>
-    </div>
+        <div>
+            <input type="checkbox" name="documentacionEntregada" id="documentacionEntregada" />
+            <label for="documentacionEntregada">Documentación entregada</label>
+        </div>
 
-    <button>Guardar</button>
-  </section>
+        <button>Guardar</button>
+    </section>
 
-  <section>
-    <h3>Listado de Profesores</h3>
-  </section>
+    <section>
+        <h3>Listado de Profesores</h3>
+    </section>
 
 </template>
-  
+
 <script lang="ts" setup>
+
+import { ref, Ref } from 'vue';
+
+interface ITeacher {
+    teacherName: string;
+    surnames: string;
+    dni: string;
+    subjects: string[];
+    doc: boolean;
+}
+
+let techer: Ref<ITeacher> = ref({
+    teacherName: '',
+    surnames: '',
+    dni: '',
+    subjects: [],
+    doc: false
+});
+
+const teachers: Ref<Array<ITeacher>> = ref([]);
+
+let subject: Ref<string> = ref('');
+
 </script>
-  
-<style scoped>
-</style>
+
+<style scoped></style>
