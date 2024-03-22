f8fc35602c296e6173a85cf3c139c1b3224d3f26
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-03-22 14:59:22 -0400

    Aplicando estilos a los formularios

--- teachers-project/src/App.vue
+++ teachers-project/src/App.vue
@@ -1,5 +1,10 @@
 <template>
-  <TeacherForm />
+
+  <div class="container">
+    <h1>Formulario de Profesores</h1>
+    <TeacherForm />
+  </div>
+  
 </template>
 
 <script lang="ts" setup>
@@ -7,4 +12,71 @@ import TeacherForm from './components/TeacherForm.vue';
 </script>
 
 <style>
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+
+    & ::before, & ::after {
+        box-sizing: inherit;
+    }
+}
+
+html {
+    font-size: 62.5%;
+}
+
+body {
+    font-size: 1.6rem;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+
+h1, h2, h3, h4, h5, h6, p {
+    margin: 0;
+}
+
+ul, ol {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+}
+
+a {
+    text-decoration: none;
+}
+
+button {
+    cursor: pointer;
+    border: 0;
+    outline: 0;
+    text-transform: uppercase;
+    padding: 1rem 2rem;
+    font-weight: 700;
+    background-color: #2c3e50;
+    color: white;
+    border-radius: 1rem;
+    font-size: 1.6rem;
+    transition: all .3s ease-in-out;
+
+    &:hover {
+        background-color: #34495e;
+    }
+
+}
+
+h1{
+    text-align: center;
+    margin-bottom: 2rem;
+    color: #34495e;
+    text-transform: uppercase;
+    text-decoration: underline;
+}
+
+.container{
+    width: min(90%, 1200px);
+    margin: 2rem auto;
+    border: .5rem solid #34495e;
+    border-radius: 1rem;
+    padding: 2rem;
+}
 </style>
--- teachers-project/src/components/TeacherForm.vue
+++ teachers-project/src/components/TeacherForm.vue
@@ -1,42 +1,49 @@
 <template>
-    <section>
+    <section class="teacher-form">
         <h3>Añadir Profesor</h3>
 
-        <div>
-            <label for="name">Nombre</label>
-            <input id="name" type="text" placeholder="Nombre" v-model="teacher.teacherName" />
-        </div>
+        <div class="fields">
 
-        <div>
-            <label for="apellidos">Apellidos</label>
-            <input id="apellidos" type="text" placeholder="Apellidos" v-model="teacher.surnames" />
-        </div>
+            <div>
+                <label for="name">Nombre</label>
+                <input id="name" type="text" placeholder="Nombre" v-model="teacher.teacherName" />
+            </div>
 
-        <div>
-            <label for="dninif">DNI/NIF</label>
-            <input id="dninif" type="text" placeholder="DNI/NIF" v-model="teacher.dni" />
-        </div>
+            <div>
+                <label for="apellidos">Apellidos</label>
+                <input id="apellidos" type="text" placeholder="Apellidos" v-model="teacher.surnames" />
+            </div>
 
-        <div>
-            <label for="materias">Materias</label>
-            <input id="materias" type="text" placeholder="Materias" v-model="subject" />
+            <div>
+                <label for="dninif">DNI/NIF</label>
+                <input id="dninif" type="text" placeholder="DNI/NIF" v-model="teacher.dni" />
+            </div>
 
-            <button @click="addSubject">Anadir</button>
+            <div class="subjects">
+                <label for="materias">Materias</label>
+                <div class="subject-input">
+                    <input id="materias" type="text" placeholder="Materias" v-model="subject" />
 
-            <ul v-if="teacher.subjects.length > 0">
-                <li v-for="(subject, index) in teacher.subjects" :key="index">{{ subject }} <button @click="removeSubject(index)">x</button></li>
-            </ul>
-        </div>
+                    <button @click="addSubject">Añadir</button>
+                </div>
 
-        <div>
-            <input type="checkbox" name="documentacionEntregada" id="documentacionEntregada" v-model="teacher.doc" />
-            <label for="documentacionEntregada">Documentación entregada</label>
+                <ul v-if="teacher.subjects.length > 0">
+                    <li v-for="(subject, index) in teacher.subjects" :key="index">{{ subject }} <button
+                            @click="removeSubject(index)">x</button></li>
+                </ul>
+            </div>
+
+            <div class="documentacion">
+                <input type="checkbox" name="documentacionEntregada" id="documentacionEntregada"
+                    v-model="teacher.doc" />
+                <label for="documentacionEntregada">Documentación entregada</label>
+            </div>
         </div>
 
         <button @click="handleAddTeacher">Guardar</button>
     </section>
 
-    <section v-if="teachers.length > 0">
+    <section v-if="teachers.length > 0" class="teacher-list">
         <h3>Listado de Profesores</h3>
 
         <table>
@@ -56,7 +63,7 @@
                     <td>{{ teacher.surnames }}</td>
                     <td>{{ teacher.dni }}</td>
                     <td>{{ teacher.subjects.join(', ') }}</td>
-                    <td>{{ teacher.doc ? 'Si' : 'No' }}</td>
+                    <td>{{ teacher.doc ? 'Sí' : 'No' }}</td>
                 </tr>
             </tbody>
         </table>
@@ -109,10 +116,140 @@ const handleAddTeacher = () => {
         subjects: teacher.value.subjects,
         doc: teacher.value.doc
     });
-    
+
     teacher.value = { ...initialTeacher, subjects: [] };
 }
 
 </script>
 
-<style scoped></style>
+<style scoped>
+h3 {
+    text-align: center;
+    color: #34495e;
+    text-transform: uppercase;
+}
+
+.teacher-form {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+
+    & .fields {
+        display: grid;
+        grid-template-columns: 1fr;
+        gap: 1rem;
+
+        @media screen and (min-width: 768px) {
+            grid-template-columns: 1fr 1fr;
+        }
+
+        div {
+            display: flex;
+            flex-direction: column;
+
+            & label {
+                color: #34495e;
+                text-transform: uppercase;
+                margin-bottom: .5rem;
+            }
+
+            & input {
+                padding: .5rem;
+                border-radius: .5rem;
+                border: 1px solid #34495e;
+                font-size: 1.6rem;
+                font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+
+                &:focus {
+                    outline: 1px solid #34495e;
+                }
+            }
+
+            & .subject-input {
+                flex-direction: row;
+                gap: .5rem;
+
+                & input {
+                    flex-grow: 1;
+                }
+
+                & button {
+                    padding: .5rem 2rem;
+                    font-size: 1.6rem;
+                }
+            }
+
+            & ul {
+                text-align: center;
+                margin: 1rem 0;
+                border: #34495e .3rem solid;
+                border-radius: .5rem;
+
+                & li {
+                    color: #34495e;
+                    padding: .5rem;
+                    display: flex;
+                    justify-content: space-between;
+                    align-items: center;
+
+                    & button {
+                        background-color: rgb(114, 0, 0);
+                        padding: .5rem 1rem;
+                        border-radius: .5rem;
+                        font-size: 1.2rem;
+
+                        &:hover {
+                            background-color: rgb(160, 0, 0);
+                        }
+                    }
+                }
+            }
+        }
+
+        & .documentacion {
+            flex-direction: row;
+            gap: .5rem;
+            align-items: center;
+
+            & label {
+                padding: 0 0 .1rem;
+                margin: 0;
+                text-transform: capitalize;
+            }
+
+            & input {
+                accent-color: #34495e;
+                border: none;
+                outline: none;
+            }
+        }
+
+
+    }
+}
+
+.teacher-list {
+
+    margin: 2rem 0;
+    overflow-x: auto;
+
+    & table {
+        width: 100%;
+        border-collapse: collapse;
+        margin-top: 2rem;
+        border: .5rem solid #34495e;
+        
+        & thead {
+            background-color: #34495e;
+            color: white;
+        }
+
+        & th, & td {
+            padding: 1rem;
+            text-align: center;
+        }
+        
+    }
+
+}
+</style>
