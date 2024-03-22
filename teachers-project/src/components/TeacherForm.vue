9c92385e535fa6507a47b134f9373c63775d21c7
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-03-21 21:51:12 -0400

    Creando el HTML para el listado de profesores

--- teachers-project/src/components/TeacherForm.vue
+++ teachers-project/src/components/TeacherForm.vue
@@ -1,12 +1,45 @@
 <template>
-    <h1>
-      Hola
-    </h1>
-  </template>
+  <section>
+    <h3>Añadir Profesor</h3>
+
+    <div>
+      <label for="name">Nombre</label>
+      <input id="name" type="text" placeholder="Nombre" />
+    </div>
+
+    <div>
+      <label for="apellidos">Apellidos</label>
+      <input id="apellidos" type="text" placeholder="Apellidos" />
+    </div>
+
+    <div>
+      <label for="dninif">DNI/NIF</label>
+      <input id="dninif" type="text" placeholder="DNI/NIF" />
+    </div>
+
+    <div>
+      <label for="materias">Materias</label>
+      <input id="materias" type="text" placeholder="Materias" />
+
+      <button>Anadir</button>
+    </div>
+
+    <div>
+        <input type="checkbox" name="documentacionEntregada" id="documentacionEntregada" />
+        <label for="documentacionEntregada">Documentación entregada</label>
+    </div>
+
+    <button>Guardar</button>
+  </section>
+
+  <section>
+    <h3>Listado de Profesores</h3>
+  </section>
+
+</template>
   
-  <script lang="ts" setup>
-  </script>
+<script lang="ts" setup>
+</script>
   
-  <style>
-  </style>
-  
+<style scoped>
+</style>
