6d9df5c50843125a71c2fff6738e6500e7784685
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-04-01 16:11:31 -0400

    Mejorando el Script Setup

--- helloworld/src/components/PostDetail.vue
+++ helloworld/src/components/PostDetail.vue
@@ -6,36 +6,32 @@
     <button @click="handleClick">Acceder</button>
   </div>
 </template>
-  
+
 <script lang="ts" setup>
-import { ref, defineProps, defineEmits } from 'vue';
+import { Ref, ref, defineProps, defineEmits } from 'vue';
 
 const props = defineProps({
   title: { type: String, required: true },
   content: { type: String, required: false, default: 'Default lorem ipsum dolor sit amet consectetur adipisicing elit.' },
 });
 
-const emit = defineEmits(['show-alert']);
+const emits = defineEmits(['show-alert']);
 
-const msg = ref('');
+let msg: Ref<string> = ref('');
 
 const handleClick = (): void => {
-  emit('show-alert', msg.value);
+  emits('show-alert', msg.value);
   msg.value = '';
 };
-
-
 </script>
 
 <style scoped>
-
-.post{
-    width: 300px;
-    height: 200px;
-    background-color: antiquewhite;
-    border-radius: 10px;
-    margin: 10px;
-    padding: 10px;
+.post {
+  width: 300px;
+  height: 200px;
+  background-color: antiquewhite;
+  border-radius: 10px;
+  margin: 10px;
+  padding: 10px;
 }
 </style>
-  
