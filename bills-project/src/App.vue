a018b0941a0ab7f752287b3d85afe4f8025635ee
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-05-10 18:21:53 -0400

    Terminando el proyecto de la factura para entender los valores computados

--- bills-project/src/App.vue
+++ bills-project/src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <h1>Hello World!</h1>
+  <BillComponent />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
+import BillComponent from './components/BillComponent.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    
+    BillComponent
   }
 });
 </script>
---
+++ bills-project/src/components/BillComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+
+        <label for="vat">IVA</label>
+        <input id="vat" type="number" v-model="vat">
+
+        <br>
+
+        <label for="subtotal">Subtotal</label>
+        <input id="subtotal" type="number" v-model="subtotal">
+
+        <h3>Total Impuestos a Pagar: {{ totalVat }}$ ({{ vat }}%)</h3>
+
+        <h3>Total a Pagar: {{ total }}$</h3>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { Ref, ref, computed } from 'vue';
+let vat = ref(21);
+let subtotal: Ref<number> = ref(0);
+
+const totalVat = computed(() => {
+    return subtotal.value * vat.value / 100;
+})
+
+const total = computed(() => {
+    return subtotal.value + totalVat.value
+})
+</script>
+
+<style scoped></style>
