f6074b19ab6ac5b11a2b63bb1accfaece7827bb1
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-05-10 17:52:44 -0400

    Computando datos desde el option api

--- helloworld/src/components/ComputedExample.vue
+++ helloworld/src/components/ComputedExample.vue
@@ -1,7 +1,10 @@
 <template>
     <div>
+        <h2>Datos Computados desde OptionAPI</h2>
         <input type="text" v-model="message">
         <h3>{{ message }}</h3>
+        <h3>{{ reverseMessage }}</h3>
+        <hr>
     </div>
 </template>
 
@@ -11,11 +14,28 @@ export default defineComponent({
     name: 'ComputedExample',
     data() {
         return {
-           message: "" 
+            message: ""
         }
     },
+    computed: {
+        reverseMessage(): string {
+            return this.message.split('').reverse().join('');
+        }
+    }
 })
 </script>
 
 <style scoped>
+    input{
+        padding: 5px 15px;
+        margin: 1rem 0;
+    }
+
+    h3{
+        margin: 1rem 0;
+        color: #2c3e50;
+    }
+    hr{
+        margin: 1rem 0;
+    }
 </style>
