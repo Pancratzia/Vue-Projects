821f2817c864c8fd67720f9fe7cd7b8370ed4d0f
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-05-10 18:10:01 -0400

    Iniciando el proyecto de las facturas

---
+++ bills-project/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Hello World!</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    
+  }
+});
+</script>
+
+<style>
+</style>
