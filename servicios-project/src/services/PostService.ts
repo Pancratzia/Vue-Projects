8e976aa898481587c5293a7c9f769cd35b90a1f2
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-05-12 12:06:13 -0400

    Desarrollando el Servicio. Falta realizar el tipado de TypeScript

---
+++ servicios-project/src/services/PostService.ts
@@ -0,0 +1,29 @@
+import { ref  } from "vue";
+
+class PostService {
+  private posts;
+
+  constructor() {
+    this.posts = ref([]);
+  }
+
+  getPosts() {
+    return this.posts;
+  }
+
+  async fetchAll() {
+    try {
+      const url = "https://jsonplaceholder.typicode.com/posts";
+
+      const response = await fetch(url);
+
+      const data = await response.json();
+
+      this.posts.value = await data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+export default PostService;
