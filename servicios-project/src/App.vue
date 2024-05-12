49f8cb612a2a306f5c14f81b76198a1b94faff63
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-05-12 19:05:47 -0400

    Eliminando PostListOption

--- servicios-project/src/App.vue
+++ servicios-project/src/App.vue
@@ -1,11 +1,9 @@
 <template>
-  <PostListOption />
   <PostList />
 </template>
 
 <script lang="ts" setup>
 import PostList from './components/PostList.vue';
-import PostListOption from './components/PostListOption.vue';
 
 
 </script>
