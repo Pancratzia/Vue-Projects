d237873348f93ae382990175aa80c2d3df0945c7
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-05-13 16:24:27 -0400

    Creando una seccion de ruteo dinamica

---
+++ routing-project/src/views/BlogView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>
+            Blog View
+        </h1>
+
+        <ul v-for="post in postsIds" :key="post">
+            <li> <router-link :to="`/blog/${post}`">Post {{ post }}</router-link> </li>
+        </ul>
+    </div>
+</template>
+
+<script lang="ts" setup>
+//Genrate a random number of post id greater than five
+const postsIds = Math.floor(Math.random() * 10) + 5;
+
+</script>
+
+<style scoped></style>
---
+++ routing-project/src/views/PostView.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        Post View
+    </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped>
+</style>
