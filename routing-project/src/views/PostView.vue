92dc7a56589774fdd43fea95dd562531e183ea45
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-05-13 16:34:10 -0400

    Se hace la lectura de los parametros desde el componente

--- routing-project/src/views/PostView.vue
+++ routing-project/src/views/PostView.vue
@@ -1,11 +1,27 @@
 <template>
     <div>
         Post View
+
+        <h1>Post {{ postId }}</h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam exercitationem illo autem ipsum! Facilis illo
+            nesciunt eum hic, est quae dignissimos animi doloremque soluta totam dolorem minus consequuntur esse cumque
+            numquam, iure alias accusamus. Est, aliquam? Id repudiandae deserunt commodi aperiam earum impedit quasi nam
+            quod. Consectetur, modi voluptatum illum aut impedit quasi dolor recusandae!</p>
+        <router-link :to="{ name: 'blog' }">Back</router-link>
     </div>
 </template>
 
 <script lang="ts" setup>
+import { onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
+
+let postId = ref('');
+
+onMounted(() => {
+    const route = useRoute();
+    postId.value = route.params.id;
+});
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
