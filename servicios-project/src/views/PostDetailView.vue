cfcbff2e198ca53754f7c8318f7356421c8203f1
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-05-13 18:29:16 -0400

    Mostrando los detalles de cada post en el PostDetailView

--- servicios-project/src/views/PostDetailView.vue
+++ servicios-project/src/views/PostDetailView.vue
@@ -1,11 +1,33 @@
 <template>
-    <div>
-        POST DETAIL
+    <h1>Post Detail #{{ post.id }}</h1>
+    <div class="card mb-2">
+
+        <div class="card-header">
+            <h3>{{ post.title }}</h3>
+        </div>
+        <div class="card-body">
+            <p>{{ post.body }}</p>
+        </div>
+
     </div>
+
+    <router-link :to="{ name: 'PostList' }" class="btn btn-secondary">Back</router-link>
 </template>
 
 <script lang="ts" setup>
+import PostService from '@/services/PostService';
+import { onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+
+const service = new PostService();
+const post = service.getPost();
+
+onMounted(async () => {
+    const route = useRoute();
+    let elm = route.params.id;
+    await service.fetchOne(elm);
+});
+
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
