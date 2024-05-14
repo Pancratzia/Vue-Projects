abc8c3bea7864a2a87ebc8f065f7f72f4818eea4
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-05-14 15:07:31 -0400

    Creando las nuevas vistas y rutas

--- auth-project/src/App.vue
+++ auth-project/src/App.vue
@@ -1,6 +1,38 @@
 <template>
-  <router-view/>
+
+  <div class="nav">
+    <router-link :to="{ name: 'home' }">Home</router-link>
+    <router-link :to="{ name: 'auth' }">Login</router-link>
+  </div>
+  <router-view />
+
 </template>
 
 <style lang="scss">
+body {
+  margin: 0;
+  padding: 0;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+}
+
+.nav {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  padding: 1rem 0;
+
+  a {
+    text-decoration: none;
+    color: #2c3e50;
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 1.2rem;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+
+  }
+}
 </style>
