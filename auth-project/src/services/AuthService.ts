b6e6d37acd913c0038130f3391d970939f615a06
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-05-14 17:23:53 -0400

    Manejando errores en el AuthService

--- auth-project/src/services/AuthService.ts
+++ auth-project/src/services/AuthService.ts
@@ -2,13 +2,19 @@ import { ref } from "vue";
 
 class AuthService {
   private jwt;
+  private error;
 
   constructor() {
-    this.jwt = ref();
+    this.jwt = ref("");
+    this.error = ref("");
   }
 
   get getJwt() {
     return this.jwt;
+  }
+
+  get getError() {
+    return this.error;
   }
 
   async login(username: string, password: string) {
@@ -22,15 +28,22 @@ class AuthService {
         },
         body: JSON.stringify({
           username: username,
-          password: password
+          password: password,
         }),
       });
 
       const data = await response.json();
 
-      if (data.token) {
-        this.jwt.value = data.token;
+      if ("message" in data && !("token" in data)) {
+        this.error.value = "Login failed";
+        return false;
       }
+
+      if ("token" in data) {
+        this.jwt.value = data.token;
+        return true;
+      }
+
     } catch (error) {
       console.log(error);
     }
