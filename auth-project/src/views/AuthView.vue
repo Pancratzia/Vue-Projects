5ba4906f10188855c0e1a0101448e78101271abc
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-05-14 17:39:12 -0400

    El login funciona correctamente con el backend tradicional

--- auth-project/src/views/AuthView.vue
+++ auth-project/src/views/AuthView.vue
@@ -1,25 +1,27 @@
 <template>
     <div class="container">
         <h1 class="text-center my-3">Login</h1>
-        <h3 class="text-center">Auth Data Obtained from <a class="link text-decoration-none" href="https://dummyjson.com/docs/auth" target="_blank">dummyJson</a></h3>
+        <h3 class="text-center">Auth Data Obtained from <a class="link text-decoration-none"
+                href="https://dummyjson.com/docs/auth" target="_blank">dummyJson</a></h3>
         <form>
             <div class="mb-3">
                 <label for="username" class="form-label">Usuario</label>
                 <input type="text" class="form-control" id="username" aria-describedby="username" v-model="username">
-                <p class="bg-danger text-white px-1 error mt-1" v-if="errors.username">{{ errors.username }}</p>
+                <p class="bg-danger text-white px-1 error mt-1" v-if="fe_errors.username">{{ fe_errors.username }}</p>
             </div>
             <div class="mb-3">
                 <label for="password" class="form-label">Contraseña</label>
                 <input type="password" class="form-control" id="password" v-model="password">
-                <p class="bg-danger text-white px-1 error mt-1" v-if="errors.password">{{ errors.password }}</p>
+                <p class="bg-danger text-white px-1 error mt-1" v-if="fe_errors.password">{{ fe_errors.password }}</p>
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-primary" @click.prevent="authUser">Submit</button>
         </form>
     </div>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue';
+import AuthService from '@/services/AuthService';
 
 const initialErrors = {
     username: '',
@@ -28,15 +30,43 @@ const initialErrors = {
 
 let username = ref('')
 let password = ref('')
-let errors = ref(initialErrors);
+let fe_errors = ref(initialErrors);
 
 
+const authUser = async () => {
 
+    if (username.value === '') {
+        fe_errors.value.username = 'El usuario es requerido';
+    } else {
+        fe_errors.value.username = '';
+    }
+
+    if (password.value === '') {
+        fe_errors.value.password = 'La contraseña es requerida';
+    } else {
+        fe_errors.value.password = '';
+    }
+
+
+    if (fe_errors.value.username === '' && fe_errors.value.password === '') {
+
+        const auth = new AuthService();
+        const success = await auth.login(username.value, password.value);
+
+        if (success) {
+            alert('Login correcto')
+            username.value = '';
+            password.value = '';
+        } else {
+            alert('Login incorrecto')
+        }
+    }
+}
 
 </script>
 
 <style scoped>
-.error{
+.error {
     font-size: 12px;
 }
 </style>
