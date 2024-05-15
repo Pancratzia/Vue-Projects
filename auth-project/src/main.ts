a43d3129c7a14030e93c9fd9f4bbf6a629f5f1f1
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-05-15 14:59:02 -0400

    Inicializando e integrando firebase al proyecto

--- auth-project/src/main.ts
+++ auth-project/src/main.ts
@@ -1,7 +1,21 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+import { initializeApp } from "firebase/app";
 
-createApp(App).use(router).mount('#app')
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_APIKEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.VUE_APP_FIREBASE_APPID,
+};
+
+console.log(firebaseConfig);
+
+initializeApp(firebaseConfig);
+
+createApp(App).use(router).mount("#app");
