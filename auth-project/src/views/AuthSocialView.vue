7606be79a4fe67ae0d3578e166d659a329f37ab4
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-05-15 18:46:17 -0400

    Agregando codigo para hacer Auth con github

--- auth-project/src/views/AuthSocialView.vue
+++ auth-project/src/views/AuthSocialView.vue
@@ -11,7 +11,7 @@
         <button @click="loginWithFacebook" class="text-center text-uppercase btn btn-primary d-block mx-auto my-3">Login
             with Facebook</button>
 
-        <button class="text-center text-uppercase btn btn-dark d-block mx-auto my-3">Login with Github</button>
+        <button @click="loginWithGithub" class="text-center text-uppercase btn btn-dark d-block mx-auto my-3">Login with Github</button>
 
         <button @click="loginWithTwitter" class="text-center text-uppercase btn btn-info d-block mx-auto my-3">Login with Twitter</button>
 
@@ -20,11 +20,12 @@
 
 <script lang="ts" setup>
 
-import { getAuth, GoogleAuthProvider, FacebookAuthProvider, TwitterAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, FacebookAuthProvider, TwitterAuthProvider, GithubAuthProvider, signInWithPopup } from "firebase/auth";
 
 const googleProvider = new GoogleAuthProvider();
 const facebookProvider = new FacebookAuthProvider();
 const twitterProvider = new TwitterAuthProvider();
+const githubProvider = new GithubAuthProvider();
 const auth = getAuth();
 
 const loginWithGoogle = () => {
@@ -62,6 +63,21 @@ const loginWithTwitter = () => {
         .then((result) => {
             console.log(result)
             //const credential = TwitterAuthProvider.credentialFromResult(result);
+            //const token = credential?.accessToken;
+            alert('Login correcto')
+        })
+        .catch((error) => {
+            console.log(error)
+            alert('Login incorrecto')
+        })
+}
+
+const loginWithGithub = () => {
+
+    signInWithPopup(auth, githubProvider)
+        .then((result) => {
+            console.log(result)
+            //const credential = GithubAuthProvider.credentialFromResult(result);
             //const token = credential?.accessToken;
             alert('Login correcto')
         })
