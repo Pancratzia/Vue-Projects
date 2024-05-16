84cc639ac0b34b3248cdd263b29242002cd373ca
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-05-16 14:59:12 -0400

    Definiendo la estructura del counter en Hello world (No funcional con errores por ahora)

--- global-vuex/src/store/index.ts
+++ global-vuex/src/store/index.ts
@@ -2,13 +2,16 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    counter: 1
   },
   getters: {
+    doubleCounter(state) {
+      return state.counter * 2
+    }
   },
   mutations: {
+    setCounter(state, payload) {
+      state.counter = payload;
+    }
   },
-  actions: {
-  },
-  modules: {
-  }
 })
