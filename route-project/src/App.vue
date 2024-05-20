fa6ddd35c78730aac0710340a01afa187eabcf5e
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-05-20 19:19:39 -0400

    Aprendiendo a enviar el token jwt a una peticion que requiera autenticacion

--- route-project/src/App.vue
+++ route-project/src/App.vue
@@ -1,8 +1,40 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <input type="text" placeholder="Información de la nota" v-model="note" />
+  <button @click="sendToServer">Enviar Nota</button>
 </template>
 
+<script lang="ts" setup>
+
+import { ref, Ref } from 'vue';
+
+let note: Ref<string> = ref('');
+
+const sendToServer = async () => {
+  if (note.value === '') {
+    alert('La nota no puede estar vacía');
+    return;
+  }
+
+  try {
+    const rawResponse = await fetch('http://XXXXXXXXXX', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': 'Bearer jwtxxxxxxxxxxxx',
+      },
+      body: JSON.stringify({ note: note.value }),
+    });
+
+    const response = await rawResponse.json();
+
+    if (response.error) {
+      alert(response.error);
+    } else {
+      alert('Nota enviada correctamente');
+    }
+  } catch (error) {
+    alert(error);
+  }
+}
+</script>
