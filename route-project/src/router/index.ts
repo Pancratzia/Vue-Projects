a8f96232d08469a6f310f062b76e9498fb96533a
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-05-20 19:54:28 -0400

    Creando un pequeño ejemplo de rutas protegidas

--- route-project/src/router/index.ts
+++ route-project/src/router/index.ts
@@ -1,35 +1,59 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
-import LoginView from '@/views/LoginView.vue'
-import DashboardView from '@/views/DashboardView.vue'
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import AboutView from "../views/AboutView.vue";
+import LoginView from "@/views/LoginView.vue";
+import DashboardView from "@/views/DashboardView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "home",
+    component: HomeView,
+    meta: {
+      requireAuth: false,
+    },
   },
   {
-    path: '/about',
-    name: 'about',
-    component: AboutView
+    path: "/about",
+    name: "about",
+    component: AboutView,
+    meta: {
+      requireAuth: false,
+    },
   },
   {
-    path: '/login',
-    name: 'login',
-    component: LoginView
+    path: "/login",
+    name: "login",
+    component: LoginView,
+    meta: {
+      requireAuth: false,
+    },
   },
   {
-    path: '/dashboard',
-    name: 'dashboard',
-    component: DashboardView
-  }
-]
+    path: "/dashboard",
+    name: "dashboard",
+    component: DashboardView,
+    meta: {
+      requireAuth: true,
+    },
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  const auth = false;
+
+  const needAuth = to.meta.requireAuth;
+
+  if (needAuth && !auth) {
+    next({ name: "login" });
+  } else {
+    next();
+  }
+});
+
+export default router;
