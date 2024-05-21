dc6512b6cedba98bcd4a31652727aadf9c9ba5fa
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-05-21 16:44:20 -0400

    Generando parte de las request del store de auth

--- auth-project-with-backend/src/store/auth.ts
+++ auth-project-with-backend/src/store/auth.ts
@@ -1,25 +1,86 @@
-import {defineStore} from "pinia";
+import { defineStore } from "pinia";
 
 const useAuth = defineStore("auth", {
-    state: () => {
-        return {
-            token: null,
-        };
+  state: () => {
+    return {
+      token: null,
+      baseUrl: "http://127.0.0.1:8000/api",
+    };
+  },
+  actions: {
+    async register(name: string, email: string, password: string) {
+      const uri = `${this.baseUrl}/register`;
+
+      const rawResponse = await fetch(uri, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: name,
+          email: email,
+          password: password,
+        }),
+      });
+
+      const response = await rawResponse.json();
+
+      // TODO: Manage response
     },
-    actions: {
-        async register(name: string, email: string, password: string) {
-            return 0;
+    async login(email: string, password: string) {
+      const uri = `${this.baseUrl}/login`;
+
+      const rawResponse = await fetch(uri, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
-        async login(email: string, password: string) {
-            return 0;
-        },
-        async getNotes() {
-           return 0; 
-        },
-        async createNotes() {
-            return 0;
-        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      });
+
+      const response = await rawResponse.json();
+
+      // TODO: Manage response
     },
+    async getNotes() {
+      const uri = `${this.baseUrl}/note`;
+
+      const rawResponse = await fetch(uri, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: `Bearer ${this.token}`,
+        },
+      });
+
+      const response = await rawResponse.json();
+
+      // TODO: Manage response
+    },
+    async createNotes(content: string) {
+      const uri = `${this.baseUrl}/note`;
+
+      const rawResponse = await fetch(uri, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: `Bearer ${this.token}`,
+        },
+        body: JSON.stringify({
+          content: content,
+        }),
+      });
+
+      const response = await rawResponse.json();
+
+      // TODO: Manage response
+    },
+  },
 });
 
 export default useAuth;
