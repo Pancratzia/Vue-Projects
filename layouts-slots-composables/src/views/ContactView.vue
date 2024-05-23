671bf2e7da5497ecc16c0011aff27e86b34c348f
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-05-23 13:18:59 -0400

    Limpiando el proyecto y creando nuevas rutas

---
+++ layouts-slots-composables/src/views/ContactView.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        Contact View
+    </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped>
+</style>
---
+++ layouts-slots-composables/src/views/ServicesView.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        Services View
+    </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped>
+</style>
