e552ad2de89e990187f5ff5af4d414c380baea0e
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-05-24 16:28:00 -0400

    Aplicando una animación individual

--- transitions-project/src/App.vue
+++ transitions-project/src/App.vue
@@ -1,13 +1,28 @@
 <template>
-  <h1>Hello World</h1>
+
+  <button @click="show = !show">Mostrar/Ocultar</button>
+  <Transition>
+    <h1 v-if="show">Hello World</h1>
+  </Transition>
 </template>
 
 
 <script lang="ts" setup>
+ import {ref} from 'vue';
 
+ const show = ref(false);
 
 </script>
 
 
-<style>
+<style scoped>
+
+.v-enter-active, .v-leave-active{
+  transition: opacity 1s ease;
+}
+
+.v-enter-from, .v-leave-to {
+  opacity: 0;
+}
+
 </style>
