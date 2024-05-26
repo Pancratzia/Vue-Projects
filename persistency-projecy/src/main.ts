6296bfcc824d7b6489e075058c830f98cc15c5f2
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-05-26 16:00:13 -0400

    Agregando pinia y bootstrap al proyecto

--- persistency-projecy/src/main.ts
+++ persistency-projecy/src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+import { createPinia } from 'pinia'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+
+const pinia = createPinia();
+
+app.use(router).use(pinia).mount('#app');
