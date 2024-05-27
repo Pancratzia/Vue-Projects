4b6f070adcbec603e847db471d7cca8f9e2dfd4e
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-05-27 17:38:56 -0400

    Usando Watch y WatchEffect para persistir los datos

--- persistency-projecy/src/App.vue
+++ persistency-projecy/src/App.vue
@@ -9,14 +9,41 @@
       <button class="btn btn-warning" @click="store.setCounter('reset')">Reset</button>
     </div>
   </div>
+
+  <div class="container my-5">
+    <h1 class="text-center">Notes</h1>
+    <h2>My note: {{ note }} </h2>
+    
+    <input class="form-control" type="text" v-model="note" placeholder="Write your note">
+
+    <h2>My age: {{ age }} </h2>
+    <input class="form-control" type="number" v-model="age" placeholder="Write your age">
+  </div>
 </template>
 
 
 
 <script lang="ts" setup>
+import { ref, Ref, watchEffect, watch } from 'vue';
 import { useExample } from './store/useExample';
 
 const store = useExample();
-
 store.init();
+
+const note: Ref<string> = ref<string>(localStorage.getItem('note') || '');
+  const age: Ref<number> = ref<number>(parseInt(localStorage.getItem('age') || '0'));
+
+
+watch(age, () => {
+  localStorage.setItem('age', age.value.toString());
+});
+
+watchEffect(() => {
+  if (note.value !== '') {
+    localStorage.setItem('note', note.value.toString());
+  } else {
+    localStorage.removeItem('note');
+  }
+});
+
 </script>
