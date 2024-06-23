ca46beb9b233561a5d7653d7c41d99aab29c037d
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-06-23 11:51:22 -0400

    Creando los componentes y limpiando el proyecto para aprender sobre control delegado por referencia

---
+++ control-project/src/components/ChildComponent.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        Child Component
+    </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped>
+</style>
---
+++ control-project/src/components/ParentComponent.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Parent Component</h1>
+
+        <ChildComponent />
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+import ChildComponent from './ChildComponent.vue';
+</script>
