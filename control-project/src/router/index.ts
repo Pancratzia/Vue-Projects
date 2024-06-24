a28107cc2c1fcb72d198f9f47aa19aed5f0584a0
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-06-24 17:15:27 -0400

    Haciendo code splitting con webchunk

--- control-project/src/router/index.ts
+++ control-project/src/router/index.ts
@@ -1,11 +1,15 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: () => import(/* webpackChunkName: "home" */ '../views/HomeView.vue')
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
 ]
 
---
+++ control-project/src/views/AboutView.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        ABOUT COMPONENT
+    </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped>
+</style>
