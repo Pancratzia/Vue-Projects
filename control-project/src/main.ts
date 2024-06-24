2a4a3bcaa278b4bba9ba58453ac549add2546655
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-06-24 16:37:25 -0400

    Añadiendo el lazy loading

--- control-project/src/main.ts
+++ control-project/src/main.ts
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import VueLazyLoad from "vue3-lazyload";
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+
+app.use(VueLazyLoad, {});
+app.use(router).mount("#app");
