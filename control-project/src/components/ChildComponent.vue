cc2941fe62b7f437f1764329bf5e8688785bcb5a
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-06-24 16:14:33 -0400

    Aprendiendo sobre control delegado

--- control-project/src/components/ChildComponent.vue
+++ control-project/src/components/ChildComponent.vue
@@ -1,11 +1,32 @@
 <template>
-    <div>
-        Child Component
-    </div>
+
+    <h4>Estado de la tarea: <span>{{ percentage }}%</span></h4>
+
 </template>
 
 <script lang="ts" setup>
+import { onMounted, ref, Ref, defineExpose } from 'vue';
+
+const percentage: Ref<number> = ref(0);
+
+onMounted(() => {
+    percentage.value = ref(25);
+})
+
+const recalculate = () => {
+    percentage.value = 50;
+}
+
+defineExpose({ recalculate });
 </script>
 
 <style scoped>
+h4 {
+    color: #2c3e50;
+
+    span {
+        color: #42b983;
+        font-weight: 700;
+    }
+}
 </style>
--- control-project/src/components/ParentComponent.vue
+++ control-project/src/components/ParentComponent.vue
@@ -1,12 +1,20 @@
 <template>
     <div>
-        <h1>Parent Component</h1>
+        <ChildComponent ref="percentageComponent"></ChildComponent>
 
-        <ChildComponent />
+        <button @click="banco">Añadir Cuenta Bancaria</button>
     </div>
 </template>
 
 <script lang="ts" setup>
-
 import ChildComponent from './ChildComponent.vue';
+import { ref } from 'vue';
+const percentageComponent = ref(null);
+
+const banco = () => {
+
+    //Conecta con el Servicio y sube la información
+    alert("Se ha añadido una cuenta bancaria");
+    percentageComponent.value.recalculate();
+}
 </script>
