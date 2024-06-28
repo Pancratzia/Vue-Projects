80fce3a1925a307ef4e1d50f09e2d253e0a14bf5
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-06-28 17:32:38 -0400

    Comenzando con la configuracion para utilizar apollo client

--- graph-apollo-client/src/main.ts
+++ graph-apollo-client/src/main.ts
@@ -1,4 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core';
 
-createApp(App).mount('#app')
+
+const httpLink = createHttpLink({
+    uri: 'http://localhost:4000/graphql',
+});
+
+const cache = new InMemoryCache();
+
+const app = createApp(App);
+
+const apolloClient = new ApolloClient({
+    link: httpLink,
+    cache
+});
+
+
+app.mount('#app')
